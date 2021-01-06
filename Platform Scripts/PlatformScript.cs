a62460611261d6169351eab81428fb8f5c265b93
Nayomal Lakshitha <nmoragane@gmail.com>
2021-01-07 00:43:50 +0530

    basic plaforms created

---
+++ Platform Scripts/PlatformScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+
+    public float move_Speed = 2f;
+    public float bound_Y = 6f;
+    public bool moving_Platform_Left, moving_Platform_Right, is_Breakable, is_Spike, is_Platform;
+
+    private Animator anim; 
+
+    private void Awake() {
+        if(is_Breakable)
+            anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move () {
+        Vector2 temp = transform.position;
+        temp.y += move_Speed * Time.deltaTime;
+        transform.position = temp;
+
+        if (temp.y >= bound_Y) {
+            gameObject.SetActive(false);
+        }
+    }
+
+
+
+
+
+} //class
---
+++ Platform Scripts/PlatformSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    public GameObject platformPrefab;
+    public GameObject spikePlatformPrefab;
+    public GameObject movingPlatform;
+    public GameObject breakablePlatform;
+
+    public float platform_Spawn_Timer = 2f;
+    private float current_Platform_Spawn_Timer;
+    private int platform_Spawn_Count;
+    public float min_X = -2f, max_X = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
