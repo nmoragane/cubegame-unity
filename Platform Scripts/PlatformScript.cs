8a6c1d0b28cb16fa1655a7dd12c673d25b32ae49
Nayomal Lakshitha <nmoragane@gmail.com>
2021-01-08 23:30:23 +0530

    othr

--- Platform Scripts/PlatformScript.cs
+++ Platform Scripts/PlatformScript.cs
@@ -40,6 +40,41 @@ public class PlatformScript : MonoBehaviour
         gameObject.SetActive(false); 
     }
 
+    void OnTriggerEnter2D (Collider2D target) {
+        if (target.tag == "Player") {
+            if(is_Spike){
+                target.transform.position = new Vector2(1000f, 1000f);
+                //SoundManager.instance.GameOverSound();
+                //GameManager.instance.RestartGame();
+
+            }
+        }
+    } // on triger enter
+
+    void OnCollisionEnter2D (Collision2D target ) {
+        if(target.gameObject.tag == "Player") {
+            if(is_Breakable) {
+                //SoundManager.instance.LandSound();
+                anim.Play("Break");
+            }
+            if(is_Platform) {
+                //SoundManager.instance.GameOverSound();
+
+            }
+        }
+    } // on collision enter
+
+    void private void OnCollisionStay2D(Collision2D target) {
+        if(target.gameObject.tag == "Player") {
+            if(moving_Platform_Left){
+                target.gameObject.GetComponent<PlayerMovement>().PlatformMove(-1f);
+            }
+            if(moving_Platform_Right){
+                target.gameObject.GetComponent<PlayerMovement>().PlatformMove(1f);
+            }
+        }
+    } //on collision stay
+
 
 
 
